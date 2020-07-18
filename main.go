b24baf02af0912b9e13bcb33340fae696bed645f
kielkow <matheuskiel@fiorifer.com.br>
2020-07-18 14:55:51 -0300

    Rendering Templates

--- main.go
+++ main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+type Post struct {
+	Id    int
+	Title string
+	Body  string
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "start server on port 8080")
+		t := template.Must(template.ParseFiles("templates/index.html"))
+
+		if err := t.ExecuteTemplate(w, "index.html", nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 	})
 
 	fmt.Println(http.ListenAndServe(":8080", nil))
