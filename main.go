e377f90d940eeb44daf52869a1748e65fb598749
kielkow <matheuskiel@fiorifer.com.br>
2020-07-18 15:15:02 -0300

    Pass values to template

--- main.go
+++ main.go
@@ -14,9 +14,19 @@ type Post struct {
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		post := Post{Id: 1, Title: "Unamed Post", Body: "No content"}
+
+		if title := r.FormValue("title"); title != "" {
+			post.Title = title
+		}
+
+		if body := r.FormValue("body"); body != "" {
+			post.Body = body
+		}
+
 		t := template.Must(template.ParseFiles("templates/index.html"))
 
-		if err := t.ExecuteTemplate(w, "index.html", nil); err != nil {
+		if err := t.ExecuteTemplate(w, "index.html", post); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
