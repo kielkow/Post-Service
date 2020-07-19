f84b5b34855a1c79c0c55178a67f46088b0608a6
kielkow <matheuskiel@fiorifer.com.br>
2020-07-18 21:08:06 -0300

    QueryRow

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func main() {
 	r.PathPrefix("/static").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("static/"))))
 
 	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/{id}/view", ViewHandler)
 
 	fmt.Println(http.ListenAndServe(":8080", r))
 }
@@ -38,6 +39,16 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.ParseFiles("templates/index.html"))
 
 	if err := t.ExecuteTemplate(w, "index.html", ListPosts()); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func ViewHandler(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+
+	t := template.Must(template.ParseFiles("templates/view.html"))
+
+	if err := t.ExecuteTemplate(w, "view.html", GetPostById(id)); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
@@ -56,6 +67,14 @@ func ListPosts() []Post {
 	}
 
 	return items
+}
+
+func GetPostById(id string) *Post {
+	row := db.QueryRow("select * from posts where id = ?", id)
+	post := Post{}
+	row.Scan(&post.Id, &post.Title, &post.Body)
+
+	return &post
 }
 
 func checkErr(err error) {
